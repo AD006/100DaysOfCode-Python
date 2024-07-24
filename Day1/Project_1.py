03d67ec828c549548bc196ce91f3fb8608c268aa
Ashish Dhiman <ashish.dhiman006@gmail.com>
2024-07-25 01:26:19 +0530

    Add files via upload

---
+++ Day1/Project_1.py
@@ -0,0 +1,12 @@
+#Greeting of program
+print("Welcome to the Band Name Generator")
+
+#Asking User for hometown
+city=input("What's the name of city you grew up in? \n")
+
+#Asking User for pet name
+pet=input("What's your pet's name? \n")
+
+#String Concatenation
+print("Your band name could be :" city " " pet)
+
